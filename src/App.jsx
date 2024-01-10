b3c5568f3aae6435d530759ae61cfbc17611e713
MalgKa <kasprzak.malg@gmail.com>
2024-01-10 09:13:09 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>todo</h1>
+        <h2>5 tasks</h2>
+        <button>+</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
